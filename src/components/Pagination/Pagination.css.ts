0683c8886826eec2ec873ebeabf31d48ea550d1c
Wojciech Malarski <w.malarski@gmail.com>
2022-07-02 14:35:52 +0200

    🎨pagination styles updated

--- src/components/Pagination/Pagination.css.ts
+++ src/components/Pagination/Pagination.css.ts
@@ -1,8 +1,15 @@
+import { sprinkles } from "@styles/sprinkles.css";
 import { vars } from "@styles/theme.css";
 import { style } from "@vanilla-extract/css";
 
-export const container = style({
-  padding: vars.space[4],
+export const container = sprinkles({
+  alignItems: "center",
+  backgroundColor: "background",
+  display: "flex",
+  flexDirection: "row",
+  gap: "4",
+  justifyContent: "center",
+  padding: "4",
 });
 
 export const item = style({
