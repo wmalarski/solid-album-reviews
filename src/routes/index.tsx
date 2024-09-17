c1fcbf0760c84f6fdc19f4ce222f4a043fff7876
Wojciech Malarski <w.malarski@gmail.com>
2024-09-17 17:36:48 +0200

    moves components out of routes folder

---
+++ src/routes/index.tsx
@@ -0,0 +1,33 @@
+import { type RouteDefinition, createAsync } from "@solidjs/router";
+
+import { SessionProvider } from "~/contexts/SessionContext";
+import { Head } from "~/modules/common/Head";
+import { PageLayout } from "~/modules/common/Layout";
+import { TopNavbar } from "~/modules/common/TopNavbar";
+import { InsertBoard } from "~/modules/createBoard/InsertBoard";
+import { getSessionLoader } from "~/server/auth/client";
+import { getInsertBoardArgsLoader } from "~/server/board/client";
+
+export const route = {
+	load: async () => {
+		await Promise.all([getSessionLoader(), getInsertBoardArgsLoader()]);
+	},
+} satisfies RouteDefinition;
+
+export default function Home() {
+	const session = createAsync(() => getSessionLoader());
+
+	const insertArgs = createAsync(() => getInsertBoardArgsLoader());
+
+	return (
+		<>
+			<Head />
+			<SessionProvider value={session()}>
+				<PageLayout>
+					<TopNavbar />
+					<InsertBoard initialValues={insertArgs()} />
+				</PageLayout>
+			</SessionProvider>
+		</>
+	);
+}
