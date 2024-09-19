37c03a4900b79587925524a74593ebceb8abcbde
Wojciech Malarski <w.malarski@gmail.com>
2024-09-19 17:55:09 +0200

    adds park ui

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
-	// Whether to use css reset
 	preflight: true,
-
-	// Where to look for your css declarations
-	include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
-
-	// Files to exclude
+	theme: { extend: {} },
+	presets: ["@pandacss/preset-base", "@park-ui/panda-preset"],
+	include: ["./src/**/*.{js,jsx,ts,tsx}"],
 	exclude: [],
-
-	// Useful for theme customization
-	theme: {
-		extend: {},
-	},
-
-	// The output directory for your css system
+	jsxFramework: "solid",
 	outdir: "src/styled-system",
 });
